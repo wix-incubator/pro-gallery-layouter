b766ce7ec4e0c9c2f215b0a43b3a477d750c17a0
Guy Sopher <guyso@wix.com>
2018-12-12 23:23:27 +0200

    add logs

--- src/layouter.js
+++ src/layouter.js
@@ -79,6 +79,22 @@ export default class Layouter {
     return minCol;
   }
 
+  logExistingLayout() {
+    console.log('Existing Layout', {
+      pointer: this.pointer,
+      layoutItems: this.layoutItems,
+      groups: this.groups,
+      strips: this.strips,
+      groupIdx: this.groupIdx,
+      groupItems: this.groupItems,
+      group: this.group,
+      strip: this.strip,
+      gallerySize: this.gallerySize,
+      galleryHeight: this.galleryHeight,
+      columns: this.columns,
+    });
+  }
+
   saveExistingLayout() {
     if (this.useLayoutStore) {
       layoutsStore.layout = ({
@@ -206,6 +222,8 @@ export default class Layouter {
     }
 
     this.prepareLayoutParams();
+
+    this.logExistingLayout();
 
     while (this.srcItems[this.pointer]) {
 
